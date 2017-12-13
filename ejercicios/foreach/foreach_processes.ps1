62b32872f67fa97c46870b96e47f2cf22a2096c1
ikerlandajuela <ikernaix@gmail.com>
2017-12-13 09:45:54 +0100

    .

--- ejercicios/foreach/foreach_processes.ps1
+++ ejercicios/foreach/foreach_processes.ps1
@@ -1,4 +1,5 @@
-# 
+# Gets the processes that are running on the local computer or a remote computer.
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-process?view=powershell-5.1
 $Proc = Get-Process
 
 ForEach ($IndProc in $Proc)
