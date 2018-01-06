b0f4b11e69bd97214d42bd7e29b86861b71463bb
ikerlandajuela <ikernaix@gmail.com>
2018-01-06 15:06:02 +0100

    Deployed 2d69ecd with MkDocs version: 0.17.2

---
+++ src/sintaxis/variables/IntVars.ps1
@@ -0,0 +1,15 @@
+$segundosPorDia = 24 * 60 * 60
+$semanasPorAnio = 365 / 7
+
+$myNum = 1
+$myNum = $myNum + 1
+# es equivalente a 
+$myNum++
+
+# Esto produce error
+#$myNum**
+
+# Equivalente a  $myNum = $myNum*2 
+$myNum*=2 
+$myNum+=2 
+
