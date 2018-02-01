e5dc796be5c8aea0e36e1215e1065be793937c27
ikerlandajuela <ikernaix@gmail.com>
2018-02-01 11:03:05 +0100

    .

--- site/src/sintaxis/bucles/while/while-basic-user-menu.ps1
+++ site/src/sintaxis/bucles/while/while-basic-user-menu.ps1
@@ -1,26 +1,43 @@
+if (!(Test-Path "test.txt"))
+{
+   New-Item -name test.txt -type "file" -value "my new text"
+   Write-Host "Created new file and text content added"
+}
+
 Clear-Host
 
-Write-Host "Menu:"
-Write-Host "d - borrar fichero."
-Write-Host "v - mostrar contenido de fichero"
-Write-Host "q - salir / quit"
+do {
+	Write-Host "============== MENU =========================="
+	Write-Host "d - borrar fichero."
+	Write-Host "v - mostrar contenido de fichero"
+	Write-Host "q - salir / quit"
+	
+	$OpcionUser = Read-Host "Selecciona opción de menú"
 
-while(($inp = Read-Host -Prompt "Select a command") -ne "q")
-{
-	switch($inp)
+	switch($OpcionUser)
 	{
-		'd' 
+		'd'
 		{
 			Write-Host "Borrado fichero"
+			Remove-Item test.txt
 		}
+		
 		'v' 
 		{
 			Write-Host "Mostrar contenido de fichero"
+			Get-Content test.txt
 			
-		}				
+		}
+		
+		'q'
+		{
+			"Saliendo"
+		}
+		
 		default 
 		{
 			Write-Host  "Invalid entry"
 		}
 	} #End switch
-} #End While
+} while($OpcionUser -ne "q")
+
